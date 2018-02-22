4c5cf54d5f67527a6eb5b62b64d23cb98f89ffb6
ZixiXu <zixixu@stanford.edu>
2018-02-22 11:20:48 -0800

    Changed output

--- CheXNet/modelSetting/net.py
+++ CheXNet/modelSetting/net.py
@@ -59,7 +59,7 @@ def loss_fn(outputs, labels):
 
 # Here is our customized
 def accuracy(outputs, label):
-    np.savetxt('output.txt', outputs, delimiter = " ", fmt = '%1d')
+    np.savetxt('output.txt', outputs - label, delimiter = " ", fmt = '%1d')
     return (1 - np.count_nonzero(np.linalg.norm(outputs - label, axis = 1)) / outputs.shape[0]) 
 
 def total_accuracy(outputs, labels):
