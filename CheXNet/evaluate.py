6fd035fb5b0fcb9479253a51f43082ddcb1ef33a
ZixiXu <zixixu@stanford.edu>
2018-03-04 14:00:58 -0800

    Minor changes

--- CheXNet/evaluate.py
+++ CheXNet/evaluate.py
@@ -104,7 +104,7 @@ def evaluate(model, dataloader, metrics, loss_fn, use_gpu):
     metrics_string = " ; ".join("{}: {:05.3f}".format(k, v) for k, v in metrics_mean.items())
     logging.info("- Eval metrics : " + metrics_string)
 
-    auc = computeROC_AUC(preds, labels) 
+    auc = net.computeROC_AUC(preds, labels) 
     logging.info("ROC AUC is :")
     logging.info(auc)
     metrics_mean['auc_mean'] = np.mean(auc)
